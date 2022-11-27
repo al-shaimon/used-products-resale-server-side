9bccdccc9c2bdb8783e28b6c1141f79cde2ad61e
al-shaimon <alshaimon968@gmail.com>
2022-11-28 03:37:18 +0600

    mongodb user pass env setup done

--- index.js
+++ index.js
@@ -14,19 +14,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const uri =
-  'mongodb+srv://<username>:<password>@cluster0.zeydczn.mongodb.net/?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zeydczn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db('test').collection('devices');
-  // perform actions on the collection object
-  client.close();
-});
+
 
 app.get('/', async (req, res) => {
   res.send('Diamond Tech server is running');
