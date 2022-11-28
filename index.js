c8e35390437132cbdba808cef2d13956167369b1
al-shaimon <alshaimon968@gmail.com>
2022-11-28 09:17:03 +0600

    jwt setup completed

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -59,6 +58,17 @@ async function run() {
 
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
+    });
+
+    app.get('/jwt', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN);
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: '' });
     });
 
     app.post('/users', async (req, res) => {
